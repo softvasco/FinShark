898604c8e2a2d7b9fcb485143f1c57369aa2ddf7
softvasco <sprvasco@hotmail.com>
2024-01-25 18:07:44 +0000

    Entity Framework

---
+++ api/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextoptions)
+            : base(dbContextoptions)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70ba91d4bd619dfb0d7bb481afdfa256b9f5490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb353015e8b50a0d7d7bc96738e9b0ffb879b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
